cfed1551408cea2ed69b7c5947568ff893ef2dcb
Matthew McMahon <msmcmahon01@gmail.com>
2024-06-24 12:00:14 -0700

    Add mod to repo

---
+++ src/client/java/marxalous/the_great_rebalance/TheGreatRebalanceClient.java
@@ -0,0 +1,10 @@
+package marxalous.the_great_rebalance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheGreatRebalanceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
