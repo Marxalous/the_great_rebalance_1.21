a5873cc58aa620425a7b6783fc4021790808f7e4
Matthew McMahon <msmcmahon01@gmail.com>
2024-06-25 08:07:44 -0700

    Add initial commit of ModBlocks

--- src/main/java/marxalous/the_great_rebalance/TheGreatRebalance.java
+++ src/main/java/marxalous/the_great_rebalance/TheGreatRebalance.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 public class TheGreatRebalance implements ModInitializer {
 	public static final String MOD_ID = "the_great_rebalance";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
